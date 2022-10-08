405866e52a1f6aebe4c13f3403125526a9dd5dc5
Jayasankar J N <55084240+Jayasankar-JN@users.noreply.github.com>
2022-10-08 15:10:57 +0530

    Update fillforms.py

--- fillforms.py
+++ fillforms.py
@@ -1,3 +1,11 @@
 from selenium import webdriver
 import random
 import time
+from selenium.webdriver.common.by import By
+
+option  = webdriver.ChromeOptions()
+#option.add_argument("-incognito")
+
+browser = webdriver.Chrome(executable_path='C:\\Users\\Desktop\\Projects\\Python\\AutoForms\\chromedriver.exe', options = option)
+browser.get("https://docs.google.com/forms/d/e/1FAIpQLSe2Yagqm8HIi_UTVG1eUyDoCMJSyrwUnr9-G2s/viewform")
+count = 1
